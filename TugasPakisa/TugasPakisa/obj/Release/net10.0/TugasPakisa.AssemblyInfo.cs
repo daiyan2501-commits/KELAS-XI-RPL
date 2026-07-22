edfbb47bd5d384a2b22bcd19b906dcccf8fdc91a
daiyan2501 <daiyan2501@gmail.com>
2026-07-22 11:02:28 +0700

    Add files via upload

--- TugasPakisa/TugasPakisa/obj/Release/net10.0/TugasPakisa.AssemblyInfo.cs
+++ TugasPakisa/TugasPakisa/obj/Release/net10.0/TugasPakisa.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("TugasPakisa")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54b946e2d2c128c05fe6f95dd640047d04730f6c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TugasPakisa")]
