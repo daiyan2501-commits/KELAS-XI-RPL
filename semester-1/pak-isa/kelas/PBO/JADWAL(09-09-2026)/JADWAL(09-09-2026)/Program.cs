b162f7189826198f78faf0f27965ab6b9be4a4d8
daiyan2501 <daiyan2501@gmail.com>
2026-09-09 09:34:42 +0700

    Add files via upload

---
+++ semester-1/pak-isa/kelas/PBO/JADWAL(09-09-2026)/JADWAL(09-09-2026)/Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+namespace Jadwal
+{ 
+    class Home
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("===== JADWAL HARI SENIN =====");
+                Senin senin = new Senin();
+                Console.WriteLine("Sesi 1:");
+                senin.sesi1();
+                Console.WriteLine("Sesi 2:");
+                senin.sesi2();
+                Console.WriteLine("Sesi 3:");
+                senin.sesi3();
+
+                Console.WriteLine();
+
+                Console.WriteLine("===== JADWAL HARI SELASA =====");
+                Selasa selasa = new Selasa();
+                Console.WriteLine("Sesi 1:");
+                selasa.sesi1();
+                Console.WriteLine("Sesi 2:");
+                selasa.sesi2();
+                Console.WriteLine("Sesi 3:");
+                selasa.sesi3();
+
+                Console.WriteLine();
+
+                Console.WriteLine("===== JADWAL HARI RABU =====");
+                Rabu rabu = new Rabu();
+                Console.WriteLine("Sesi 1:");
+                rabu.sesi1();
+                Console.WriteLine("Sesi 2:");
+                rabu.sesi2();
+                Console.WriteLine("Sesi 3:");
+                rabu.sesi3();
+
+                Console.WriteLine();
+
+                Console.WriteLine("===== JADWAL HARI KAMIS =====");
+                Kamis kamis = new Kamis();
+                Console.WriteLine("Sesi 1:");
+                kamis.sesi1();
+                Console.WriteLine("Sesi 2:");
+                kamis.sesi2();
+                Console.WriteLine("Sesi 3:");
+                kamis.sesi3();
+
+                Console.WriteLine();
+
+                Console.WriteLine("===== JADWAL HARI JUMAT =====");
+                Jumat jumat = new Jumat();
+                Console.WriteLine("Sesi 1:");
+                jumat.sesi1();
+                Console.WriteLine("Sesi 2:");
+                jumat.sesi2();
+
+                Console.WriteLine();
+                Console.WriteLine("Tekan ENTER untuk keluar...");
+                Console.ReadLine();
+            }
+        }
+
+    }
+    class Senin
+    {
+        public void sesi1()
+        {
+            Console.WriteLine("Upacara");
+            Console.WriteLine("Bahasa Jepang");
+            Console.WriteLine("Bahasa Inggris");
+        }
+        public void sesi2() 
+        {
+            Console.WriteLine("Matematika");
+        }
+        public void sesi3()
+        {
+            Console.WriteLine("PPLG");
+        }
+    }
+    class Selasa
+    {
+        public void sesi1()
+        {
+            Console.WriteLine("PKWU");
+            Console.WriteLine("PJOK");
+        }
+        public void sesi2()
+        {
+            Console.WriteLine("Bahasa Indonesia");
+        }
+        public void sesi3()
+        {
+            Console.WriteLine("PPLG");
+        }
+    }
+    class Rabu
+    {
+        public void sesi1()
+        {
+            Console.WriteLine("PPLG");
+        }
+        public void sesi2()
+        {
+            Console.WriteLine("PPLG");
+        }
+        public void sesi3()
+        {
+            Console.WriteLine("PPLG");
+            Console.WriteLine("BK");
+        }
+    }
+    class Kamis
+    {
+        public void sesi1()
+        {
+            Console.WriteLine("Bahasa Jerman");
+            Console.WriteLine("Bahasa Jawa");
+        }
+        public void sesi2()
+        {
+            Console.WriteLine("PKWU");
+        }
+        public void sesi3()
+        {
+            Console.WriteLine("PAI");
+        }
+    }
+    class Jumat
+    {
+        public void sesi1()
+        {
+            Console.WriteLine("Bahasa Inggris");
+            Console.WriteLine("Sejarah");
+        }
+        public void sesi2()
+        {
+            Console.WriteLine("Pendidikan Pancasila");
+        }
+    }
+
+
+
+
+
+}
---
+++ semester-1/pak-isa/kelas/PBO/PBO/PBO/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+namespace Matematika
+{
+    class Home
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Belajar PBO");
+
+            Kalkulator kal = new Kalkulator();
+            BangunDatar BDatar = new BangunDatar();
+            BangunRuang BRuang = new BangunRuang();
+            kal.penjumlahan();
+            kal.pengurangan();
+            kal.perkalian();
+
+        }
+
+    }
+    class Kalkulator
+    {
+        public void penjumlahan()
+        {
+            Console.WriteLine("Penjumlahan");
+        }
+
+        public void pengurangan()
+        {
+            Console.WriteLine("Pengurangan");
+        }
+
+        public void perkalian()
+        {
+            Console.WriteLine("Perkalian");
+        }
+
+    }
+
+    class BangunDatar
+    {
+        public static void KelilingPersegiPanjang()
+        {
+            Console.WriteLine("Keliling Persegi Panjang");
+        }
+
+        public static void KelilingLingkaran()
+        {
+            Console.WriteLine("Keliling Lingkaran");
+        }
+
+        public static void LuasPersegiPanjang()
+        {
+            Console.WriteLine("Luas Persegi Panjang");
+        }
+
+        public static void LuasLingkaran()
+        {
+            Console.WriteLine("Luas Lingkaran");
+        }
+    }
+
+    class BangunRuang
+    { 
+        public void VolumeBalok()
+        {
+            Console.WriteLine("Volume Balok");
+        }
+
+        public void VolumeTabung()
+        {
+            Console.WriteLine("Volume Tabung");
+        }
+
+    }
+
+}
+
+
+
+
+
+
