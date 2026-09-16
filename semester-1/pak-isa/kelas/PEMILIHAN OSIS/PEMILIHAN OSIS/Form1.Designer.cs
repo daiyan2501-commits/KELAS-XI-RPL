86b4d0cfed9519c6e00f901e8b75fadb186983af
daiyan2501 <daiyan2501@gmail.com>
2026-09-16 10:59:35 +0700

    Add files via upload

---
+++ semester-1/pak-isa/kelas/PEMILIHAN OSIS/PEMILIHAN OSIS/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace PEMILIHAN_OSIS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblJudul = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.rbCalon1 = new System.Windows.Forms.RadioButton();
+            this.rbCalon2 = new System.Windows.Forms.RadioButton();
+            this.rbCalon3 = new System.Windows.Forms.RadioButton();
+            this.picMirza = new System.Windows.Forms.PictureBox();
+            this.picAntok = new System.Windows.Forms.PictureBox();
+            this.picWiji = new System.Windows.Forms.PictureBox();
+            this.lblCalon1 = new System.Windows.Forms.Label();
+            this.lblMirza = new System.Windows.Forms.Label();
+            this.lblAntok = new System.Windows.Forms.Label();
+            this.lblWiji = new System.Windows.Forms.Label();
+            this.btnVote = new System.Windows.Forms.Button();
+            this.lblCalon2 = new System.Windows.Forms.Label();
+            this.lblCalon3 = new System.Windows.Forms.Label();
+            this.lblTotalVote = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picMirza)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picAntok)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picWiji)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblJudul
+            // 
+            this.lblJudul.AutoSize = true;
+            this.lblJudul.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblJudul.Location = new System.Drawing.Point(103, 16);
+            this.lblJudul.Name = "lblJudul";
+            this.lblJudul.Size = new System.Drawing.Size(198, 19);
+            this.lblJudul.TabIndex = 0;
+            this.lblJudul.Text = "PEMILIHAN KETUA OSIS";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblJudul);
+            this.panel1.Location = new System.Drawing.Point(-3, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(395, 50);
+            this.panel1.TabIndex = 1;
+            // 
+            // rbCalon1
+            // 
+            this.rbCalon1.AutoSize = true;
+            this.rbCalon1.Location = new System.Drawing.Point(24, 296);
+            this.rbCalon1.Name = "rbCalon1";
+            this.rbCalon1.Size = new System.Drawing.Size(86, 17);
+            this.rbCalon1.TabIndex = 4;
+            this.rbCalon1.TabStop = true;
+            this.rbCalon1.Text = "Pilih Calon 1 ";
+            this.rbCalon1.UseVisualStyleBackColor = true;
+            // 
+            // rbCalon2
+            // 
+            this.rbCalon2.AutoSize = true;
+            this.rbCalon2.Location = new System.Drawing.Point(143, 296);
+            this.rbCalon2.Name = "rbCalon2";
+            this.rbCalon2.Size = new System.Drawing.Size(83, 17);
+            this.rbCalon2.TabIndex = 4;
+            this.rbCalon2.TabStop = true;
+            this.rbCalon2.Text = "Pilih Calon 2";
+            this.rbCalon2.UseVisualStyleBackColor = true;
+            // 
+            // rbCalon3
+            // 
+            this.rbCalon3.AutoSize = true;
+            this.rbCalon3.Location = new System.Drawing.Point(265, 296);
+            this.rbCalon3.Name = "rbCalon3";
+            this.rbCalon3.Size = new System.Drawing.Size(83, 17);
+            this.rbCalon3.TabIndex = 4;
+            this.rbCalon3.TabStop = true;
+            this.rbCalon3.Text = "Pilih Calon 3";
+            this.rbCalon3.UseVisualStyleBackColor = true;
+            // 
+            // picMirza
+            // 
+            this.picMirza.Image = ((System.Drawing.Image)(resources.GetObject("picMirza.Image")));
+            this.picMirza.Location = new System.Drawing.Point(15, 148);
+            this.picMirza.Name = "picMirza";
+            this.picMirza.Size = new System.Drawing.Size(100, 87);
+            this.picMirza.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picMirza.TabIndex = 5;
+            this.picMirza.TabStop = false;
+            // 
+            // picAntok
+            // 
+            this.picAntok.Image = ((System.Drawing.Image)(resources.GetObject("picAntok.Image")));
+            this.picAntok.Location = new System.Drawing.Point(131, 148);
+            this.picAntok.Name = "picAntok";
+            this.picAntok.Size = new System.Drawing.Size(100, 87);
+            this.picAntok.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picAntok.TabIndex = 6;
+            this.picAntok.TabStop = false;
+            // 
+            // picWiji
+            // 
+            this.picWiji.Image = ((System.Drawing.Image)(resources.GetObject("picWiji.Image")));
+            this.picWiji.Location = new System.Drawing.Point(256, 148);
+            this.picWiji.Name = "picWiji";
+            this.picWiji.Size = new System.Drawing.Size(100, 87);
+            this.picWiji.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picWiji.TabIndex = 6;
+            this.picWiji.TabStop = false;
+            // 
+            // lblCalon1
+            // 
+            this.lblCalon1.AutoSize = true;
+            this.lblCalon1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCalon1.Location = new System.Drawing.Point(18, 102);
+            this.lblCalon1.Name = "lblCalon1";
+            this.lblCalon1.Size = new System.Drawing.Size(94, 19);
+            this.lblCalon1.TabIndex = 0;
+            this.lblCalon1.Text = "CALON1 : 0";
+            // 
+            // lblMirza
+            // 
+            this.lblMirza.AutoSize = true;
+            this.lblMirza.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMirza.Location = new System.Drawing.Point(40, 238);
+            this.lblMirza.Name = "lblMirza";
+            this.lblMirza.Size = new System.Drawing.Size(42, 17);
+            this.lblMirza.TabIndex = 0;
+            this.lblMirza.Text = "Mirza";
+            // 
+            // lblAntok
+            // 
+            this.lblAntok.AutoSize = true;
+            this.lblAntok.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAntok.Location = new System.Drawing.Point(159, 238);
+            this.lblAntok.Name = "lblAntok";
+            this.lblAntok.Size = new System.Drawing.Size(44, 17);
+            this.lblAntok.TabIndex = 0;
+            this.lblAntok.Text = "Antok";
+            // 
+            // lblWiji
+            // 
+            this.lblWiji.AutoSize = true;
+            this.lblWiji.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWiji.Location = new System.Drawing.Point(290, 238);
+            this.lblWiji.Name = "lblWiji";
+            this.lblWiji.Size = new System.Drawing.Size(30, 17);
+            this.lblWiji.TabIndex = 0;
+            this.lblWiji.Text = "Wiji";
+            // 
+            // btnVote
+            // 
+            this.btnVote.Location = new System.Drawing.Point(143, 363);
+            this.btnVote.Name = "btnVote";
+            this.btnVote.Size = new System.Drawing.Size(75, 23);
+            this.btnVote.TabIndex = 7;
+            this.btnVote.Text = "VOTE";
+            this.btnVote.UseVisualStyleBackColor = true;
+            this.btnVote.Click += new System.EventHandler(this.btnVote_Click);
+            // 
+            // lblCalon2
+            // 
+            this.lblCalon2.AutoSize = true;
+            this.lblCalon2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCalon2.Location = new System.Drawing.Point(125, 102);
+            this.lblCalon2.Name = "lblCalon2";
+            this.lblCalon2.Size = new System.Drawing.Size(94, 19);
+            this.lblCalon2.TabIndex = 0;
+            this.lblCalon2.Text = "CALON2 : 0";
+            // 
+            // lblCalon3
+            // 
+            this.lblCalon3.AutoSize = true;
+            this.lblCalon3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCalon3.Location = new System.Drawing.Point(252, 102);
+            this.lblCalon3.Name = "lblCalon3";
+            this.lblCalon3.Size = new System.Drawing.Size(94, 19);
+            this.lblCalon3.TabIndex = 0;
+            this.lblCalon3.Text = "CALON3 : 0";
+            // 
+            // lblTotalVote
+            // 
+            this.lblTotalVote.AutoSize = true;
+            this.lblTotalVote.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalVote.Location = new System.Drawing.Point(116, 65);
+            this.lblTotalVote.Name = "lblTotalVote";
+            this.lblTotalVote.Size = new System.Drawing.Size(124, 19);
+            this.lblTotalVote.TabIndex = 0;
+            this.lblTotalVote.Text = "TOTAL VOTE: 0";
+           
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(391, 450);
+            this.Controls.Add(this.btnVote);
+            this.Controls.Add(this.lblWiji);
+            this.Controls.Add(this.lblAntok);
+            this.Controls.Add(this.lblMirza);
+            this.Controls.Add(this.lblCalon3);
+            this.Controls.Add(this.lblTotalVote);
+            this.Controls.Add(this.lblCalon2);
+            this.Controls.Add(this.lblCalon1);
+            this.Controls.Add(this.picWiji);
+            this.Controls.Add(this.picAntok);
+            this.Controls.Add(this.picMirza);
+            this.Controls.Add(this.rbCalon3);
+            this.Controls.Add(this.rbCalon2);
+            this.Controls.Add(this.rbCalon1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picMirza)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picAntok)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picWiji)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblJudul;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton rbCalon1;
+        private System.Windows.Forms.RadioButton rbCalon2;
+        private System.Windows.Forms.RadioButton rbCalon3;
+        private System.Windows.Forms.PictureBox picMirza;
+        private System.Windows.Forms.PictureBox picAntok;
+        private System.Windows.Forms.PictureBox picWiji;
+        private System.Windows.Forms.Label lblCalon1;
+        private System.Windows.Forms.Label lblMirza;
+        private System.Windows.Forms.Label lblAntok;
+        private System.Windows.Forms.Label lblWiji;
+        private System.Windows.Forms.Button btnVote;
+        private System.Windows.Forms.Label lblCalon2;
+        private System.Windows.Forms.Label lblCalon3;
+        private System.Windows.Forms.Label lblTotalVote;
+    }
+}
+
---
+++ semester-1/pak-isa/kelas/PEMILIHAN OSIS/PEMILIHAN OSIS/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace PEMILIHAN_OSIS
+{
+    public partial class Form1 : Form
+    {
+        int voteCalon1 = 0;
+        int voteCalon2 = 0;
+        int voteCalon3 = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            lblCalon1.Text = "CALON 1 : 0";
+            lblCalon2.Text = "CALON 2 : 0";
+            lblCalon3.Text = "CALON 3 : 0";
+            lblTotalVote.Text = "TOTAL VOTE: 0";
+        }
+
+        private void btnVote_Click(object sender, EventArgs e)
+        {
+            if (rbCalon1.Checked)
+            {
+                voteCalon1++;
+                lblCalon1.Text = "CALON 1 : " + voteCalon1;
+            }
+            else if (rbCalon2.Checked)
+            {
+                voteCalon2++;
+                lblCalon2.Text = "CALON 2 : " + voteCalon2;
+            }
+            else if (rbCalon3.Checked)
+            {
+                voteCalon3++;
+                lblCalon3.Text = "CALON 3 : " + voteCalon3;
+            }
+            else
+            {
+                MessageBox.Show("Silakan pilih calon terlebih dahulu!");
+                return;
+            }
+
+            // Menghitung total semua vote
+            int totalVote = voteCalon1 + voteCalon2 + voteCalon3;
+
+            // Menampilkan total semua vote
+            lblTotalVote.Text = "TOTAL VOTE: " + totalVote;
+
+            // Menghapus pilihan setelah vote
+            rbCalon1.Checked = false;
+            rbCalon2.Checked = false;
+            rbCalon3.Checked = false;
+        }
+    }
+}
